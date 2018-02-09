687800942e00ace230575896a015390e36efb8bc
John Baker <phibonacci85@gmail.com>
2018-02-08 21:45:28 -0600

    put on github

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
-import { RecipeService } from '../recipe.service';
+import {RecipeService} from '../recipe.service';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
@@ -12,14 +12,16 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private recipeService: RecipeService,
-              private router: Router,
-              private route: ActivatedRoute) { }
+  constructor(
+    private recipeService: RecipeService,
+    private router: Router,
+    private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
-        this.id = +params['id']
+        this.id = +params['id'];
         this.recipe = this.recipeService.getRecipeById(this.id);
       }
     );
