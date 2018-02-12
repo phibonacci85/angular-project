199bd75842b20a08c725884a53597726ddf9643c
John Baker <phibonacci85@gmail.com>
2018-02-11 21:02:27 -0600

    Completed Auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBqiNpcnOuvB-SeZ0k1k31j1HHtJAAUyDM',
+      authDomain: 'ng-recipe-book-c4c1a.firebaseapp.com',
+    });
+  }
+
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,24 +1,28 @@
-import {Injectable} from '@angular/core';
-import {Http, Response} from '@angular/http';
-import {RecipeService} from '../recipes/recipe.service';
-import {Recipe} from '../recipes/recipe.model';
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 import 'rxjs/add/operator/map';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
 
   constructor(
     private http: Http,
-    private recipeService: RecipeService) {
+    private recipeService: RecipeService,
+    private authService: AuthService) {
   }
 
   storeRecipes() {
-    return this.http.put('https://ng-recipe-book-c4c1a.firebaseio.com/recipes.json',
+    const token = this.authService.getToken();
+    return this.http.put('https://ng-recipe-book-c4c1a.firebaseio.com/recipes.json?auth=' + token,
       this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    this.http.get('https://ng-recipe-book-c4c1a.firebaseio.com/recipes.json').map(
+    const token = this.authService.getToken();
+    this.http.get('https://ng-recipe-book-c4c1a.firebaseio.com/recipes.json?auth=' + token).map(
       (response: Response) => {
         const recipes: Recipe[] = response.json();
         for (const recipe of recipes) {
