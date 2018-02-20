432721903d93fa2bd2c5cfbfa8faf548925090ac
John Baker <phibonacci85@gmail.com>
2018-02-20 16:30:34 -0600

    switching Http to HttpClient

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,30 +1,41 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { HttpClient, HttpHeaders, HttpParams, HttpRequest } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import 'rxjs/add/operator/map';
 import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
 
   constructor(
-    private http: Http,
+    private httpClient: HttpClient,
     private recipeService: RecipeService,
     private authService: AuthService) {
   }
 
   storeRecipes() {
     const token = this.authService.getToken();
-    return this.http.put('https://ng-recipe-book-c4c1a.firebaseio.com/recipes.json?auth=' + token,
-      this.recipeService.getRecipes());
+    // return this.httpClient.put('https://ng-recipe-book-c4c1a.firebaseio.com/recipes.json',
+    //   this.recipeService.getRecipes(), {
+    //     observe: 'body',
+    //     params: new HttpParams().set('auth', token)
+    //   });
+    const req = new HttpRequest('PUT', 'https://ng-recipe-book-c4c1a.firebaseio.com/recipes.json',
+      this.recipeService.getRecipes(),
+      {
+        reportProgress: true,
+        params: new HttpParams().set('auth', token),
+      },
+    );
+    return this.httpClient.request(req);
   }
 
   getRecipes() {
     const token = this.authService.getToken();
-    this.http.get('https://ng-recipe-book-c4c1a.firebaseio.com/recipes.json?auth=' + token).map(
-      (response: Response) => {
-        const recipes: Recipe[] = response.json();
+    this.httpClient.get<Recipe[]>('https://ng-recipe-book-c4c1a.firebaseio.com/recipes.json?auth=' + token).map(
+      (recipes) => {
         for (const recipe of recipes) {
           if (!recipe['ingredients']) {
             recipe['ingredients'] = [];
